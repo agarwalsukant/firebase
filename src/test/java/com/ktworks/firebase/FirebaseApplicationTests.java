b8e683cef8b1a157f54f469fe57d2e88e63c1005
Sukant <sukant.su@gmail.com>
2023-06-24 00:24:39 +0530

    Created Project to learn Firebase

---
+++ src/test/java/com/ktworks/firebase/FirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktworks.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
